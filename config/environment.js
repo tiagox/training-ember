f4623dfcbe61bc5620969d8c3d673df635cce45e
Santiago Rojo <tiagox@gmail.com>
2015-05-06 18:11:19 -0300

    Change the name of the app.

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-training',
+    modulePrefix: 'training-ember',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
